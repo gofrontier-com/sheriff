1aaae46df2ea04d5c668e07c09cff9d936e71c12
Craig Anderson <1877372+CDA0@users.noreply.github.com>
2023-02-18 13:14:15 +0000

    for each

--- pkg/cmd/cli/validate/validate.go
+++ pkg/cmd/cli/validate/validate.go
@@ -1,33 +1,43 @@
 package validate
 
 import (
-	"github.com/frontierdigital/sheriff/pkg/util/validation"
-	"github.com/spf13/cobra"
-	// 	hashiVers "github.com/hashicorp/go-version"
-	// 	"github.com/hashicorp/hc-install/product"
-	// 	"github.com/hashicorp/hc-install/releases"
-	// 	"github.com/hashicorp/terraform-exec/tfexec"
-)
+	"log"
+	"os"
+	"path/filepath"
 
-var (
-	inputDir = ""
+	"github.com/frontierdigital/sheriff/pkg/util/validation"
+	"github.com/frontierdigital/utils/output"
+	"github.com/spf13/cobra"
 )
 
 // NewCmdValidate creates a command to output the current version of Sheriff
 func NewCmdValidate() *cobra.Command {
 	c := &cobra.Command{
-		Use:   "validate",
+		Use:   "validate groups-path",
 		Short: "Validate will check input data for correctness",
 		Long:  "Looks at input files and returns true or false if valid data or not",
-		RunE: func(_ *cobra.Command, _ []string) error {
+		Args:  cobra.ExactArgs(1),
+		RunE: func(_ *cobra.Command, args []string) error {
+			output.Println(args)
+			entries, err := os.ReadDir(args[0])
+			if err != nil {
+				log.Fatal(err)
+			}
+			output.Println(entries)
 
-			validation.ValidateConfiguration("./sre.yml")
+			for _, e := range entries {
+				f := filepath.Join(args[0], e.Name())
+				valid, err := validation.ValidateConfiguration(f)
+				if !valid {
+					output.Println("Invalid %s - %v", f, err)
+				} else {
+					output.Println("valid %s", f)
+				}
+			}
 
 			return nil
 		},
 	}
-
-	c.Flags().StringVarP(&inputDir, "input-dir", "i", "", "Directory holding the input.")
 
 	return c
 }
